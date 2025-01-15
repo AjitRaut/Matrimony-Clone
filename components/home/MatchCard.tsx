a8f57919cee0e994078536356b7af3277b79479e
Ajit raut <ajitraut9561@gmail.com>
2025-01-15 17:25:18 +0530

    AllMatches added

---
+++ components/home/MatchCard.tsx
@@ -0,0 +1,57 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+interface MatchCardProps {
+  name: string;
+  age: number;
+  height: string | number;
+}
+
+// Functional Component with Props
+const MatchCard: React.FC<MatchCardProps> = ({ name, age, height }) => {
+  return (
+    <TouchableOpacity style={styles.matchCard}>
+      <View style={styles.imageContainer}>
+        <Image
+         source={{uri:"https://imgs.bharatmatrimony.com/webapp-assets/revamp-images/female-avatar-img-new.svg"} }
+          style={styles.profileImage}
+        />
+      </View>
+      <Text style={styles.nameText}>{name}</Text>
+      <Text style={styles.detailsText}>
+        {age} Yrs, {height}"
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default MatchCard;
+
+const styles = StyleSheet.create({
+  matchCard: {
+    width: 150,
+    marginRight: 12,
+  },
+  imageContainer: {
+    width: "100%",
+    aspectRatio: 1,
+    backgroundColor: "#E0E0E0",
+    borderRadius: 8,
+    marginBottom: 8,
+    overflow: "hidden",
+  },
+  profileImage: {
+    width: "100%",
+    height: "100%",
+  },
+  nameText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#333",
+    marginBottom: 2,
+  },
+  detailsText: {
+    fontSize: 14,
+    color: "#666",
+  },
+});
