96c094d0c9851d8cf98ae0ea87fa04171bb474aa
Ajit raut <ajitraut9561@gmail.com>
2025-01-22 23:03:15 +0530

    ByCriteria tab added

---
+++ components/search/ByCriteria.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import SearchCriteria from './SearchCriteria'
+
+const ByCriteria : React.FC = () => {
+  return (
+    <View style={{flex:1}}>
+      <SearchCriteria/>
+    </View>
+  )
+}
+
+export default ByCriteria
+
+const styles = StyleSheet.create({
+
+})
