21337a2bb330d236079b617dae8503e19f152bf6
Ajit raut <ajitraut9561@gmail.com>
2025-01-23 21:52:57 +0530

    MessageScreen Added

--- components/messages/MessageScreen.tsx
+++ components/messages/MessageScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import ReceivedScreen from "./RecivedScreen";
 import AwaitingResponse from "./AwaitingResponse";
@@ -9,43 +9,39 @@ const Tab = createMaterialTopTabNavigator();
 
 const MessageScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Messages</Text>
       </View>
 
       <Tab.Navigator
         screenOptions={{
-          tabBarActiveTintColor: "#00A300",
-          tabBarInactiveTintColor: "#666",
+          tabBarActiveTintColor: "#000",
+          tabBarInactiveTintColor: "#000",
           tabBarIndicatorStyle: {
-            backgroundColor: "#00A300",
+            backgroundColor: "green",
             height: 3,
           },
           tabBarLabelStyle: {
-            fontSize: 14,
+            fontSize: 12,
             textTransform: "none",
+          },
+          tabBarStyle: {
+            backgroundColor: "#fff",
           },
         }}
       >
         <Tab.Screen name="Received" component={ReceivedScreen} />
-        <Tab.Screen
-          name="Awaiting Response"
-          component={AwaitingResponse}
-        />
+        <Tab.Screen name="Awaiting Response" component={AwaitingResponse} />
         <Tab.Screen name="Calls" component={CallsScreen} />
       </Tab.Navigator>
-    </SafeAreaView>
+    </>
   );
 };
 
 export default MessageScreen;
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
   header: {
     padding: 16,
     borderBottomWidth: 1,
